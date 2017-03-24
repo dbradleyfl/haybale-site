31277599a83dceec8dad46f50882334efb9ac30c
Devon Bradley <dbradleyfl@gmail.com>
2017-03-24 04:04:01 -0700

    move navbar into a component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="nav">
-          <div className="nav-left">
-            <a className="nav-item">
-              <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo" />
-            </a>
-          </div>
-
-          <div className="nav-center">
-            <a className="nav-item">
-              <span className="icon">
-                <i className="fa fa-github"></i>
-              </span>
-            </a>
-            <a className="nav-item">
-              <span className="icon">
-                <i className="fa fa-twitter"></i>
-              </span>
-            </a>
-          </div>
-
-          <span className="nav-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-
-          <div className="nav-right nav-menu">
-            <a className="nav-item">
-              Home
-            </a>
-            <a className="nav-item">
-              Documentation
-            </a>
-            <a className="nav-item">
-              Blog
-            </a>
-
-            <span className="nav-item">
-              <a className="button" >
-                <span className="icon">
-                  <i className="fa fa-twitter"></i>
-                </span>
-                <span>Tweet</span>
-              </a>
-              <a className="button is-primary">
-                <span className="icon">
-                  <i className="fa fa-download"></i>
-                </span>
-                <span>Download</span>
-              </a>
-            </span>
-          </div>
-        </nav>
+        <Navbar />
       </div>
     );
   }
